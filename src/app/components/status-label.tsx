40f911d1ae36ea57748a11838d0d9af57afdfb71
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2025-03-26 13:31:33 +0100

    conditional styling

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx'
+
 export enum Status {
   Active = 'active',
   NotActive = 'notActive',
@@ -8,18 +10,27 @@ export enum Status {
 export interface StatusLabelProps {
   children: React.ReactNode
   status: Status
+  disabled?: boolean
 }
 
-export default function StatusLabel({ children, status }: StatusLabelProps) {
+export default function StatusLabel({
+  children,
+  status,
+  disabled,
+}: StatusLabelProps) {
+  console.log(process.env.CRM_EXPORT_KEY)
   return (
     <span
-      className={`inline-flex items-center gap-2 py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        (status === Status.Active && 'text-green-700 bg-green-100') ||
-        (status === Status.NotActive && 'text-red-700 bg-red-100') ||
-        (status === Status.Pending && 'text-orange-700 bg-orange-100') ||
-        (status === Status.Suspended && 'text-blue-700 bg-blue-100') ||
-        ''
-      } `}
+      className={clsx(
+        'inline-flex items-center gap-2 py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Active && 'text-green-700 bg-green-100',
+        status === Status.NotActive && 'text-red-700 bg-red-100',
+        status === Status.Pending && 'text-orange-700 bg-orange-100',
+        status === Status.Suspended && 'text-blue-700 bg-blue-100',
+        {
+          ['opacity-75 cursor-not-allowed']: disabled,
+        },
+      )}
     >
       <div className="w-1 h-1 rounded-full bg-current" />
       {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <main className="">
       <h1 className="text-xl">Home page</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.NotActive} disabled>
+        Not Active
+      </StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended </StatusLabel>
     </main>
