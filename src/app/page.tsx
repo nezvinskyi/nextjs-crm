0690b126fbf2f52239550e5373b1e74672630ffa
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2025-03-26 13:35:52 +0100

    dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { headers } from 'next/headers'
 import StatusLabel, { Status } from './components/status-label'
 
 export default function Home() {
+  console.log(headers())
   return (
     <main className="">
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive} disabled>
         Not Active
