0fae502aee528f1a9f1d3e63bd17564a4685ef6e
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2025-03-26 17:39:49 +0100

    layout

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,10 +1,10 @@
-import AddCompanyButton from '../components/add-company-button'
-import CompanyRow from '../components/company-row'
-import CompanyTable from '../components/company-table'
-import Header from '../components/header'
-import SearchInput from '../components/search-input'
-import { Status } from '../components/status-label'
-import Toolbar from '../components/toolbar'
+import AddCompanyButton from '../../components/add-company-button'
+import CompanyRow from '../../components/company-row'
+import CompanyTable from '../../components/company-table'
+import Header from '../../components/header'
+import SearchInput from '../../components/search-input'
+import { Status } from '../../components/status-label'
+import Toolbar from '../../components/toolbar'
 
 export interface PageProps {
   children?: React.ReactNode
