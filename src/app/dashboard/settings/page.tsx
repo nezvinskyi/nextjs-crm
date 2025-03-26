f877ed7021dba189343245b0f47525b65f9f7aa4
Dmitry Nezvinskyi <nezvinskyi@abss.at>
2025-03-26 16:20:53 +0100

    routing

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+export interface PageProps {
+  children?: React.ReactNode
+}
+
+export default function Page({}: PageProps) {
+  return (
+    <main>
+      <h1 className="text-xl">Settings</h1>
+    </main>
+  )
+}
